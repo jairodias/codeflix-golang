8ee44d73468d8ef9f6e7736afa0967d1ca112697
jairodias <jairopereira_dias@hotmail.com>
2022-03-09 19:45:37 -0300

    feat: criando a função de job para gerenciar o passo a passo das conversões de video

--- application/services/job_service.go
+++ application/services/job_service.go
@@ -3,6 +3,9 @@ package services
 import (
 	"encoder/application/repositories"
 	"encoder/domain"
+	"errors"
+	"os"
+	"strconv"
 )
 
 type JobService struct {
@@ -12,7 +15,81 @@ type JobService struct {
 }
 
 func (j *JobService) Start() error {
+	err := j.changeJobStatus("DOWNLOADING")
+	if err != nil {
+		return j.failJob(err)
+	}
 
+	err = j.VideoService.Download(os.Getenv("inputBucketName"))
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("FRAGMENTING")
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.VideoService.Fragment()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("ENCODING")
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.VideoService.Encode()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.performUpload()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("FINISHING")
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.VideoService.Finish()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("COMPLETED")
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	return nil
+}
+
+func (j *JobService) performUpload() error {
+	err := j.changeJobStatus("UPLOADING")
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	videoUpload := NewVideoUpload()
+	videoUpload.OutpuBucket = os.Getenv("outputBucketName")
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + j.VideoService.Video.ID
+	concurrency, _ := strconv.Atoi(os.Getenv("CONCURRENCY_UPLOAD"))
+	doneUpload := make(chan string)
+
+	go videoUpload.ProccessUpload(concurrency, doneUpload)
+
+	var uploadResult string
+	uploadResult = <-doneUpload
+
+	if uploadResult != "uploaded completed" {
+		return j.failJob(errors.New(uploadResult))
+	}
+
+	return err
 }
 
 func (j *JobService) changeJobStatus(status string) error {

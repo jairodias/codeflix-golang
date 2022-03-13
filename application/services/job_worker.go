257d32c248d9987e47a369ed8b1923dc1dd5ce2c
jairodias <jairopereira_dias@hotmail.com>
2022-03-13 07:51:39 -0300

    feat: criando o consumer

--- application/services/job_worker.go
+++ application/services/job_worker.go
@@ -3,7 +3,11 @@ package services
 import (
 	"encoder/domain"
 	"encoder/framework/utils"
+	"encoding/json"
+	"os"
+	"time"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/streadway/amqp"
 )
 
@@ -13,7 +17,7 @@ type JobWorkerResult struct {
 	Error   error
 }
 
-func JobWorker(messageChannel chan amqp.Delivery, returnChannel chan JobWorkerResult, jobService JobService, workerId int) {
+func JobWorker(messageChannel chan amqp.Delivery, returnChannel chan JobWorkerResult, jobService JobService, job domain.Job, workerId int) {
 	for message := range messageChannel {
 		err := utils.IsJson(string(message.Body))
 
@@ -21,14 +25,54 @@ func JobWorker(messageChannel chan amqp.Delivery, returnChannel chan JobWorkerRe
 			returnChannel <- returnJobResult(domain.Job{}, message, err)
 			continue
 		}
+
+		err = json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		jobService.VideoService.Video.ID = uuid.NewV4().String()
+		if err != nil {
+			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.Video.Validate()
+		if err != nil {
+			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.InsertVideo()
+		if err != nil {
+			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.Job.Video
+		job.OutputBucketPath = os.Getenv("outputBucketName")
+		job.ID = uuid.NewV4().String()
+		job.Status = "STARTING"
+		job.CreatedAt = time.Now()
+
+		_, err = jobService.JobRepository.Insert(&job)
+		if err != nil {
+			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		jobService.Job = &job
+		err = jobService.Start()
+		if err != nil {
+			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		returnChannel <- returnJobResult(job, message, nil)
 	}
 }
 
 func returnJobResult(job domain.Job, message amqp.Delivery, err error) JobWorkerResult {
-	return := JobJobWorkerResult{
-		Job: job,
+	result := JobWorkerResult{
+		Job:     job,
 		Message: &message,
-		Error: err
+		Error:   err,
 	}
 
 	return result
